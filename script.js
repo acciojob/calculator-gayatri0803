d149d2f5000733bfe24c9cfe61da5b275a74a54b
gayatri0803 <85298488+gayatri0803@users.noreply.github.com>
2023-02-22 22:01:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+let input = document.getElementById("input");
+
+function add(value) {
+  input.value += value;
+}
+
+function calculate() {
+  try {
+    input.value = eval(input.value);
+  } catch (error) {
+    if (error instanceof SyntaxError) {
+      input.value = "Error";
+    }
+  }
+}
+
+function clearInput() {
+  input.value = "";
+}
+
+function reset() {
+  input.value = "";
+}
+
+function handleKeyPress(event) {
+  if (event.keyCode === 13) {
+    calculate();
+  }
+}
